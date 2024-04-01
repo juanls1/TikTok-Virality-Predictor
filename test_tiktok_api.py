c6655e7de38262d471215d60e217cccd6dc95880
juanls1 <202308780@alu.comillas.edu>
2024-04-01 14:23:22 +0200

    Funciona

--- test_tiktok_api.py
+++ test_tiktok_api.py
@@ -1,4 +1,5 @@
 from TikTokApi import TikTokApi
+import asyncio
 import os
 
 ms_token = os.environ.get(
@@ -6,25 +7,15 @@ ms_token = os.environ.get(
 )  # set your own ms_token, think it might need to have visited a profile
 
 
-async def fetch_videos():
-    api = TikTokApi()
-    async with api:
-        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
+async def foo():
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False) #headless=False is the unique change. It is needed to function properly
         videos = []
-        async for video in api.trending.videos(count=3):
+        async for video in api.trending.videos(count=5):
+            print(video.id)
             videos.append(video)
-            print("hey")
-        return videos
+    return videos
 
-
-async def debug_fetch_videos():
-    videos = await fetch_videos()
-    # Aquí puedes poner puntos de interrupción o utilizar print() para inspeccionar los vídeos
-    for video in videos:
-        print(video)
-
-import asyncio
-
-asyncio.run(debug_fetch_videos())
+videoss = asyncio.run(foo())
 
 print("done")
