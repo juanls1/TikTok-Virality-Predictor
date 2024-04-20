36090463a8d4c13b5418a488cefa7857ad3d67fe
juanls1 <202308780@alu.comillas.edu>
2024-04-20 16:44:32 +0200

    semi audio extraction

--- config/variables.py
+++ config/variables.py
@@ -2,5 +2,11 @@
 
 # Data Exploration & Visualization
 
-sample_path = 'data/inputs/audios/'
+audio_path = 'data/inputs/audios/'
 indiv_sample_path = 'data/inputs/audios/6875370613523909890_audio.mp3'
+
+
+
+# Text
+
+text_path = 'data/inputs/texts/'
